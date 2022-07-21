0a258937c751471ea83f83efa9f1474c3b4b6f70
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-07-21 18:32:13 +0400

    build(package.json): build issue fixed
    build config updated for  heroicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  build: {
+    transpile: ["@heroicons/vue"],
+  },
 });
